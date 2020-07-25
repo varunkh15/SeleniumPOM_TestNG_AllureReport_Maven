3ef12a82e3f313f808314755905e51b9d4609b50
Varun <varunkh15@gmail.com>
2020-07-25 18:13:11 +0800

    update code

--- src/main/java/com/qa/hubspot/base/BaseTest.java
+++ src/main/java/com/qa/hubspot/base/BaseTest.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.qa.hubspot.pages.LoginPage;
 
@@ -15,10 +17,12 @@ public class BaseTest {
 	public BasePage basePage;
 	public LoginPage loginPage;
 
+	@Parameters("browser")
 	@BeforeTest
-	public void setUp() {
+	public void setUp(String browserName) {
 		basePage = new BasePage();
 		prop = basePage.init_prop();
+		prop.setProperty("browser", browserName);
 		driver = basePage.init_driver(prop);
 		loginPage = new LoginPage(driver);
 	}
